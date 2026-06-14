7089b5212807d5a970334323b0e70c4f13ed81ea
OldNGreyBMR <73047349+OldNGreyBMR@users.noreply.github.com>
2026-06-15 09:01:58 +1000

    correct case of version number

--- zc_plugins/PaymentModuleFee/V2.0.4/catalog/includes/templates/template_default/auto_loaders/loader_ot_paymentmodulefee.php
+++ zc_plugins/PaymentModuleFee/V2.0.4/catalog/includes/templates/template_default/auto_loaders/loader_ot_paymentmodulefee.php
@@ -2,11 +2,11 @@
 /**
 * @package Pages
 * @copyright Copyright 2008-2009 RubikIntegration.com
-* @copyright Copyright 2003-2023 Zen Cart Development Team
+* @copyright Copyright 2003-2026 Zen Cart Development Team
 * @copyright Portions Copyright 2003 osCommerce
 * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-* @version $Id: loader_ot_paymentmodulefee.php V2.0.0 BMH (OldNGreY) 2023-01-28
- ZC158 PHP8.1 
+* @version $Id: loader_ot_paymentmodulefee.php V2.0.4 BMH (OldNGreY) 2026-06-14
+* 
 */                                             
 if (MODULE_ORDER_TOTAL_PAYMENTMODULEFEE_STATUS == 'true') {                                                            
   $loaders[] = array('conditions' => array('pages' => array('checkout', 'quick_checkout')),
